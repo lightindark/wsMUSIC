959fbf02abcc44995b72f23d96d90c767d6b545e
phi toan <neverback@neverback.(none)>
2014-03-21 01:22:52 +0700

    WSM-20 #fix file uninstall

--- component/admin/sql/uninstall.mysql.utf8.sql
+++ component/admin/sql/uninstall.mysql.utf8.sql
@@ -2,10 +2,3 @@
 DROP TABLE IF EXISTS `#__wsmusic_track` ;
 DROP TABLE IF EXISTS `#__wsmusic_artist` ;
 DROP TABLE IF EXISTS `#__wsmusic_playlist` ;
-
-DELETE gr FROM `never_user_usergroup_map` gr
-JOIN `never_users` u ON u.id=gr.user_id
-WHERE u.username = 'mabama' OR u.username = 'gayman' OR u.username = 'lamchidoan';
-
-DELETE FROM `never_users`
-WHERE username = 'mabama' OR username = 'gayman' OR username = 'lamchidoan';
