4165c0f74ec7b637174731731a4a35ff70a73590
phi toan <neverback@neverback.(none)>
2014-03-20 17:28:28 +0700

    WSM-6 #fix helper

--- component/admin/helpers/wsmusichelper.php
+++ component/admin/helpers/wsmusichelper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_content
+ * @subpackage  com_wsmusic
  *
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 /**
- * Content component helper.
+ * Wsmusic component helper.
  *
  * @package     Joomla.Administrator
  * @subpackage  com_wsmusic
@@ -18,33 +18,26 @@ defined('_JEXEC') or die;
  */
 class WsmusicHelper extends JHelperContent
 {
-	
 	/**
 	 * Configure the Linkbar.
 	 *
-	 * @param   string	$vName  The name of the active view.
+	 * @param   string  $vName  The name of the active view.
 	 *
 	 * @return  void
 	 *
 	 * @since   0.0.1
 	 */
-	public static function addSubmenu($vName = 'weblinks')
+	public static function addSubmenu($vName = 'tracks')
 	{
 		JHtmlSidebar::addEntry(
-				JText::_('COM_WSMUSIC_SUBMENU_WEBLINKS'),
-				'index.php?option=com_wsmusic&view=wsmusics',
-				$vName == 'wsmusics'
+				JText::_('COM_WSMUSIC_SUBMENU_TRACKS'),
+				'index.php?option=com_wsmusic&view=tracks',
+				$vName == 'tracks'
 		);
 		JHtmlSidebar::addEntry(
 				JText::_('COM_WSMUSICS_SUBMENU_CATEGORIES'),
-				'index.php?option=com_categories&extension=com_wsmusics',
+				'index.php?option=com_categories&extension=com_wsmusic',
 				$vName == 'categories'
 		);
-		JHtmlSidebar::addEntry(
-				JText::_('COM_WSMUSIC_SUBMENU_FEATURED'),
-				'index.php?option=com_wsmusic&view=featured',
-				$vName == 'featured'
-		);
 	}
-	
 }
