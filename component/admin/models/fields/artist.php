fcf9a5078688532723f2067c1fc0247755c1cee4
phi toan <neverback@neverback.(none)>
2014-03-23 19:54:53 +0700

    WSM-22 #fix: missing artist filter

---
+++ component/admin/models/fields/artist.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Form Field class for the Joomla Platform.
+ * Supports a generic list of options.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ * @since       11.1
+ */
+class JFormFieldArtist extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  3.2
+	 */
+	public $type = 'Artist';
+
+	/**
+	 * Cached array of the category items.
+	 *
+	 * @var    array
+	 * @since  3.2
+	 */
+	protected static $options = array();
+
+	/**
+	 * Method to get the options to populate list
+	 *
+	 * @return  array  The field option objects.
+	 *
+	 * @since   3.2
+	 */
+
+	protected function getOptions()
+	{
+		// Accepted modifiers
+		$hash = md5($this->element);
+
+		if (!isset(static::$options[$hash]))
+		{
+			static::$options[$hash] = parent::getOptions();
+
+			$options = array();
+
+			$db = JFactory::getDbo();
+
+			// Construct the query
+			$query = $db->getQuery(true)
+				->select('a.id AS value, a.name AS text')
+				->from('#__wsmusic_artist AS a')
+				->join('INNER', '#__wsmusic_track AS t ON t.artist = a.id')
+				->group('a.id, a.name')
+				->order('a.name');
+
+			// Setup the query
+			$db->setQuery($query);
+
+			// Return the result
+			if ($options = $db->loadObjectList())
+			{
+				static::$options[$hash] = array_merge(static::$options[$hash], $options);
+			}
+		}
+
+		return static::$options[$hash];
+	}
+}
